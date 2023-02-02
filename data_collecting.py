0f9e62ac8b6d58b57d492fccdc5121b127904fea
Tran Nguyen Le <tran.nguyenle@aalto.fi>
2023-02-02 21:03:25 +0200

    Modified sponge to be rigid for collecting data on gundam.

--- data_collecting.py
+++ data_collecting.py
@@ -2,7 +2,7 @@ import subprocess
 from colorama import Fore, Back, Style
 
 
-objects = ["bowl_a_shapenet","bowl_b_shapenet","bowl_c_shapenet","bowl_d_shapenet","bowl_e_shapenet",
+objects = ["bowl_ycb","plate_ycb","bowl_shapenet","bowl_a_shapenet","bowl_b_shapenet","bowl_c_shapenet","bowl_d_shapenet","bowl_e_shapenet",
            "bowl_f_shapenet", "plate_a_shapenet"]
 num_iters = 100
 for object_name in objects:
